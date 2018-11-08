03c44b3a73e14f45b41b0fa1784bb829f995117c
s124265 <44394075+s124265@users.noreply.github.com>
2018-11-08 12:26:33 +0100

    Update wordCount.py

--- wordCount.py
+++ wordCount.py
@@ -1,10 +1,29 @@
+import wikipedia
+import nltk
+from nltk.corpus import stopwords
+from collections import defaultdict
+
+
+stop = [x.lower() for x in stopwords.words('english')]
+dklist  =[]
+dk = wikipedia.page("Denmark")
+dklist.append(dk.content.split(" "))
+print(len(dklist[0]))
+filtered_dklist = [w for w in dklist[0] if not w in stop]
+print(len(filtered_dklist))
+
+
 
 class WordCount:
     
-    def WordFreq(self, article):
+    def __init__ (self,article):
         self.wordCounts = {}
-        for word in article:
-            if word in article:
+        self.wikiArt = article
+        self.wordCounts = defaultdict(int)
+    
+    def WordFreq(self):
+        for word in self.wikiArt:
+            if word in self.wikiArt:
                 self.wordCounts[word] += 1
         return self.wordCounts
     
@@ -17,3 +36,6 @@ class WordCount:
                     Category = word
         return Category
 
+
+wordC = WordCount(filtered_dklist)
+print(wordC.WordFreq())
