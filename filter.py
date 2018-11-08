a126a45ce6d78461ce2fa293e83a2846d93f08d7
Tom Althin <tom.althin@gmail.com>
2018-11-08 13:44:05 +0100

    comments for filter fuctions

--- filter.py
+++ filter.py
@@ -3,14 +3,13 @@ import nltk
 from nltk.corpus import stopwords
 
 
-def remove_all_but_words(wordlist):
-    filtered_wordlist = re.sub(r'[^a-zA-Z ]', '', wordlist).split()
+### takes a string and removes digits and nonalpha characters
+def remove_all_but_words(word_string):
+    filtered_wordlist = re.sub(r'[^a-zA-Z ]', '', word_string).split()
     return filtered_wordlist
 
-
+### takes a list of words and removes all stop words.
 def remove_stopwords(wordlist):
     stop = [x.lower() for x in set(stopwords.words('english'))]
     filtered_wordlist = [w for w in wordlist if not w in stop]
     return filtered_wordlist
-
-
