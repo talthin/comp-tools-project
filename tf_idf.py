aa17d7f3b2d113095f14045ee46d9ecd34dae9ef
Tom Althin <tom.althin@gmail.com>
2018-11-22 12:40:18 +0100

    tf-idf now takes command line arguments

--- tf_idf.py
+++ tf_idf.py
@@ -2,28 +2,38 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import mapping
 import math
 import re
+import sys
 from collections import Counter
 
 
-d = {}
-d2 = {}
+doclist = []
+for arg in sys.argv[1:]:
+    wordDict = {}
+    with open(arg) as f:
+        for line in f:
+            # print(line)
+            (key, val) = line.split()
+            wordDict[key] = int(val)
+    doclist.append(wordDict)
 
-def computeTF(wordDict):
-    tfdict = {}
-    dictCount = len(wordDict)
-    for word, count in wordDict.items():
-        tfdict[word] = count / float(dictCount)
-    return (tfdict)
+# d = {}
+# d2 = {}
+
+# def computeTF(wordDict):
+#     tfdict = {}
+#     dictCount = len(wordDict)
+#     for word, count in wordDict.items():
+#         tfdict[word] = count / float(dictCount)
+#     return (tfdict)
 
 
-# TODO, ONLY COUNT OCCURENCE ONCE PER DOCUMENT
-def computeIDF(docList):
+def computeIDF(doclist):
     idfDict = {}
-    N = len(docList)
+    N = len(doclist)
 
 
-    for i in range(len(docList)):
-        for word in docList[i]:
+    for i in range(len(doclist)):
+        for word in doclist[i]:
             if word in idfDict:
                 idfDict[word] += 1
             else:
@@ -36,45 +46,24 @@ def computeIDF(docList):
 
 def computeTF_IDF(doclist, IDFdict):
 
-    tfidf= {}
-    unionSet = dict(Counter(doclist[0]) + Counter(doclist[1]))
+    tfidf_dicts = []
+    for i in range(len(doclist)):
+        tfidf_dicts.append(doclist[i])
 
-    for word, val in unionSet.items():
-        tfidf[word] = val * IDFdict[word]
+    for wdict in tfidf_dicts:
+        for word, val in wdict.items():
+            wdict[word] = val * IDFdict[word]
 
-    return tfidf
+    # tfidf = {}
+    # unionSet = dict(Counter(doclist[0]) + Counter(doclist[1]))
+    # for word, val in unionSet.items():
+    #     tfidf[word] = val * IDFdict[word]
 
-
-with open("phymap.txt") as f:
-    for line in f:
-        # print(line)
-        (key, val) = line.split()
-        d[key] = int(val)
-
-with open("culmap.txt") as f:
-    for line in f:
-        # print(line)
-        (key, val) = line.split()
-        d2[key] = int(val)
-
-# dList = list(d.keys())
-# d2List = list(d2.keys())
-# wordset = set(dlist).union(set(d2list))
+    return tfidf_dicts
 
 
 
-computeTF(d)
-idfs = computeIDF([d, d2])
-tfidf = computeTF_IDF([d, d2], idfs)
+idfs = computeIDF(doclist)
+tfidf = computeTF_IDF(doclist, idfs)
 print(tfidf)
-
-
-# print(idfs)
-
-# print(d)
-# data1 = F1.read()
-# data2 = F2.read()
-# Fit the TfIdf model
-
-# # Transform a document into TfIdf coordinates
-# X = TfidfVectorizer.transform(data1)
+# print(tfidf[1])
