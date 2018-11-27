818ab9a98d5ac72e988dbff81a95fa27ecebe817
s124265 <44394075+s124265@users.noreply.github.com>
2018-11-27 10:30:33 +0100

    Add topTF and used in the unspecified classifier :)

--- workinprogress.py
+++ workinprogress.py
@@ -1,15 +1,27 @@
-import tf_idf
+import tf_idf_nocomm
 import filter
 import wikipedia
 
 
 
-doclist = tf_idf.tf_idf_init()
-idf = tf_idf.computeIDF(doclist)
-tfidf = tf_idf.computeTF_IDF(doclist,idf)
+No = 100
+doclist = tf_idf_nocomm.tf_idf_init()
+idf = tf_idf_nocomm.computeIDF(doclist)
+tfidf = tf_idf_nocomm.computeTF_IDF(doclist,idf)
+TopTF = tf_idf_nocomm.ToptfidfWeights(tfidf,N)
+
+testVec = []
+
+for i in range(len(TopTF)):
+    temp = [0]*No*len(TopTF)
+    for j in range(len(TopTF[i])):
+        temp[j+i*No] = 1
+    testVec.append(temp)
+    
+#print(testVec[0].count(1))
 
 #print(idf)
-
+    
 dk = wikipedia.page("Denmark")
 dk = dk.content
 article = filter.remove_all_but_words(dk)
@@ -17,43 +29,22 @@ article = filter.remove_stopwords(article)
 
 #print(len(tfidf[5]))
 
+#print(TopTF[5]["varphi"])
 
-def tf_idf_classifier(tfidf,idf,article):
-    N = len(tfidf) #Amount of clusters (aka amount of categories in MoD)
-    m = len(idf) #Amount of words in our vocabulary
+def gay_classifier(tfidf,idf,TopTF,testVec, article,No):
+    n = len(tfidf) #Amount of clusters (aka amount of categories in MoD)
+    m = No*len(TopTF) #Amount of words in our vocabulary
+    ArtVec = [0]*n*m
     
-    label = []
-    
-    dictart = {}
-    
-    for word in article:
-        if word in idf:
-            if word in dictart:
-                dictart[word] += 1
-            else:
-                dictart[word] = 1
+    for i in range(len(TopTF)):
+        for j in range(len(TopTF[i])):
+            if list(TopTF[i].keys())[j] in article:
+                ArtVec[j+i*No] = 1
+                
         
-    L = None
-    LNN = 0
-    for i in range(0,N):
-        nn = 0
-        for word in dictart:
-            #nn += (tfidf[i][word]-dictart[word]*tfidf[i][word])**2
-            if word not in tfidf[i]:
-                continue
-            else:
-                nn += tfidf[i][word]*dictart[word]
-            
-        #nn = nn**(0.5)
-        print(nn)
-        if L == None:
-            L = i
-            LNN = nn
-        elif nn > LNN:
-            L = i
-            LNN = nn
-            
-    label.append(L)
-    return label
+    return ArtVec
 
-print(tf_idf_classifier(tfidf,idf,article))
+
+
+
+print(gay_classifier(tfidf,idf,TopTF,testVec,article,No))
